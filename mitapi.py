b210660b1167bc0486e5a3f02e12a30ee0a6f652
Eric Zhong <ezhong1900@gmail.com>
2018-10-31 07:14:53 -0400

    Add files via upload

---
+++ mitapi.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from twilio.rest import Client
+from flask_restful import reqparse, abort, Api, Resource
+import pickle
+import numpy as np
+from mitmodel import cl
+app = Flask(__name__)
+api = Api(app)
+# create new model object
+model = cl
+# load trained classifier
+clf_path = '/Users/EricZhong/mit_model.pkl'
+with open(clf_path, 'rb') as f:
+    model.clf = pickle.load(f)
+
+parser = reqparse.RequestParser()
+parser.add_argument('query')
+
+class PredictStep(Resource):
+    def get(self):
+        # use parser and find the user's query
+        args = parser.parse_args()
+        user_query = args['query']
+        # vectorize the user's query and make a prediction
+        # uq_vectorized = model.vectorizer_transform(
+        #     np.array([user_query]))
+        prediction = model.classify(user_query)
+        pred_proba = model.prob_classify(user_query)
+        # Output 'Negative' or 'Positive' along with the score
+        pred_text = prediction
+
+        # round the predict proba value and set to new variable
+        confidence = str(pred_proba.prob(pred_proba.max()))
+        # create JSON object
+        output = {'prediction': pred_text, 'confidence': confidence}
+        print(output)
+
+        return output
+
+api.add_resource(PredictStep, '/predict')
+
+
+# example of another endpoint
+# api.add_resource(PredictRatings, '/ratings')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ mitmodel.py
@@ -0,0 +1,36 @@
+from sklearn.feature_selection import SelectFromModel
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from random import shuffle
+import random
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn import metrics
+import coremltools
+import time
+import pickle
+from textblob import TextBlob
+
+train = [
+      ('Whats your favorite color', 'Baby Blue'),
+      ('What do you do outside of school', 'I teach chess lessons and develop develop mobile and web apps')
+]
+
+test = [
+       ('Color', 'Baby Blue'),
+       ('Activities', 'I teach chess lessons and develop develop mobile and web apps')
+]
+
+from textblob.classifiers import NaiveBayesClassifier
+with open('mitwebanswers.csv', 'r') as fp:
+    cl = NaiveBayesClassifier(fp, format="csv")
+
+pred = cl.prob_classify("Please tell me what your favorite color is")
+print(pred.max())
+print(pred.prob(pred.max()))
+
+filename = 'mit_model.pkl'
+pickle.dump(cl, open(filename, 'wb'))
+
+print("successfuly saved model")
